f4c8dfdc55616bf6b3fa1b3eccce1ad4c775f0fc
Jay Fleming <jacobmfleming@gmail.com>
2018-02-19 17:10:07 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+
+def reverse_each_word(sentence)
+  reversed_sentence = ""
+  sentence.split.collect do |word|
+    word.reverse
+  end
+  
+
+end
+
+
+# reversed_sentence << "#{word.reverse} "
+
+# return reversed_sentence
